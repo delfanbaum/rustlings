eb08e48e3360d5bd30cd05f5c6e0e87c79ce24eb
delfanbaum <delfanbaum@oreilly.com>
2024-06-09 18:25:09 -0400

    Running through

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(q: u32) -> u32 {
+    if q <= 40 {
+        q * 2
+    } else {
+        q
+    }
+}
 
 // Don't modify this function!
 #[test]
